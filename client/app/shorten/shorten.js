645f62781d93e8cf5ba613a20f13d06e1b7ac4b2
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-24 17:11:32 -0500

    pass all tests

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,7 +1,13 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $http, $location, Links) {
   
   $scope.link = {};
+
+  $scope.addLink = function() {
+    $http.post('/api/links').success(function(data) {
+      console.log('Successfully posted link! ', data);
+    });
+  };
 
 });
