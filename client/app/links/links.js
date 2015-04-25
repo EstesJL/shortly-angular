fcb86ac811e344348611a01b9e09a510a4fed2fa
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-25 13:10:18 -0500

    write media queries for breaking point

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -11,4 +11,33 @@ angular.module('shortly.links', [])
   };
   $scope.getLinks();
 
+  $scope.link = {};
+
+  $scope.addLink = function() {
+    var url = {};
+    url.url = $scope.url;
+    Links.shortenLink(url);
+    TweenMax.to(".titlecolor", 1, {"font-size": 20, delay: 0});
+    TweenMax.to(".titlecolor", 1, {"font-size": 30, delay: 1});
+  };
+
+  TweenMax.to(".titlecolor", 2, {"font-size": 20, delay: 2});
+  TweenMax.to(".titlecolor", 1, {"font-size": 30, delay: 4});
+  TweenMax.to(".titlecolor", 1, {y: "15", delay: 5})
 });
+
+
+// angular.module('shortly.links', [])
+
+// .controller('LinksController', function ($scope, $http, Links) {
+  
+//   $scope.data = {};
+
+//   $scope.getLinks = function() {
+//     Links.fetchLinks().then(function(links) {
+//       $scope.data.links = links;
+//     });
+//   };
+//   $scope.getLinks();
+
+// });
